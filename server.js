ff416735f41b51d52c41c7aa0bccb5579a8b36f0
IY Chirkov <ly4uk84@gmail.com>
2024-03-12 08:03:45 +0400

    ready base server

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http"), fs = require('fs');
+
+fs.readFile('./server.js', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
